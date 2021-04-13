9bda70cff9948ea9c989da510de38ae543d27078
wodyd202 <wodyd202@naver.com>
2021-04-13 22:19:47 +0900

    @프로젝트 시작

---
+++ HOME-COOKING-SHARE/src/main/java/com/homecookingshare/HomeCookingShareApplication.java
@@ -0,0 +1,13 @@
+package com.homecookingshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeCookingShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeCookingShareApplication.class, args);
+	}
+
+}
---
+++ HOME-COOKING-SHARE/src/test/java/com/homecookingshare/HomeCookingShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecookingshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCookingShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
